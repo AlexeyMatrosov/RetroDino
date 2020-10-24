947375295e42d1f3d6b97e93697327e71f9773e3
Alex Matrosov <Alexey.S.Matrosov@gmail.com>
2020-10-24 04:43:37 +0300

    Add event clock (first version)

--- retrodino.asm
+++ retrodino.asm
@@ -14,11 +14,24 @@ COLOR_GREEN 		= $00
 COLOR_BLUE 			= $80
 COLOR_BACKGROUND	= $c0
 
-SMALL_DELAY 		= 30 
 clear		=	$FF
 
+CACTUS_X_REG		= 	050		; #40
+BIRD_X_REG			= 	051		; #41
 DINO_Y_REG			=	060		; #48
 DINO_STATE_REG 		= 	061		; #49
+
+
+CLOCK_DINO_ANIMATION_REG 	= 062		; #50
+CLOCK_DINO_MOVE_REG			= 063		; #51
+CLOCK_FIELD_REG				= 064		; #52
+CLOCK_BIRD_REG				= 065		; #53
+
+; Set real data
+TIMING_DINO_ANIMATION	= 3
+TIMING_DINO_MOVE		= 10
+TIMING_FIELD			= 2
+TIMING_BIRD				= 1
 	
 	org $800
 	
@@ -36,6 +49,26 @@ cartridgeEntry:
 	SETISAR DINO_Y_REG
 	lr S, A
 	
+	li TIMING_DINO_ANIMATION
+	SETISAR CLOCK_DINO_ANIMATION_REG
+	lr S, A
+	
+	li TIMING_FIELD
+	SETISAR CLOCK_FIELD_REG
+	lr S, A
+	
+	li TIMING_BIRD
+	SETISAR CLOCK_BIRD_REG
+	lr S, A
+	
+	li 110
+	SETISAR CACTUS_X_REG
+	lr S, A
+	
+	li 110
+	SETISAR BIRD_X_REG
+	lr S, A
+	
 	pi drawGround				; Draw game ground
 	pi drawSky					; Draw game sky
 	
@@ -44,43 +77,10 @@ mainloop:
 	clr
 	outs	0
 	outs	1
-	ins	1
-	com							; un-invert port data
-	ni	%00000011				; only keep push/pull
-	bz	_endloop
 	
-	ni %00000010
-	bnz _leftclicked
+	; DRAWING
 	
-	; Right clicked
-	SETISAR DINO_Y_REG
-	lr A, S
-	inc
-	lr S, A
-	
-	jmp _endloop	
-	
-_leftclicked:
-
-	; Left clicked
-	SETISAR DINO_Y_REG
-	lr A, S
-	lr 2, A
-	ds 2
-	lr A, 2
-	lr S, A
-
-	jmp _endloop
-	
-_endloop:
-	
-	SETISAR DINO_STATE_REG
-	lr A, S
-	inc
-	lr S, A
-	
-	; Draw Dino
-	
+	; Dino 
 	SETISAR DINO_Y_REG
 	lr A, S			
 	lr 1, A			; Dino y
@@ -94,7 +94,8 @@ _endloop:
 	
 	; Draw cactus 0
 	
-	li 60
+	SETISAR CACTUS_X_REG
+	lr A, S
 	lr 1, A
 	
 	li 2
@@ -102,55 +103,139 @@ _endloop:
 	
 	pi drawCactus
 	
-	; Draw cactus 1
-	
-	li 75
-	lr 1, A
-	
-	li 3
-	lr 2, A
-	
-	pi drawCactus
-	
-	; Draw cactus 2
-	
-	li 90
-	lr 1, A
-	
-	li 4
-	lr 2, A
-	
-	pi drawCactus
-	
 	; Draw bird
 	
-	li 70
+	SETISAR BIRD_X_REG
+	lr A, S
 	lr 1, A
 	
 	li 5
 	lr 2, A
 	
-	pi drawBird
+	;pi drawBird
 	
-	pi small_delay
+	; LOGIC
+	pi processDinoAnimationClock
+	pi processDinoMoveClock
+	pi processFieldClock
+	pi processBirdClock
 	
-	jmp mainloop
+	;ins	1
+	;com							; un-invert port data
+	;ni	%00000011				; only keep push/pull
+	;bz	_endloop
 	
-; uses r8, A
-small_delay:
-	li SMALL_DELAY
-	lr 8, A
+	;ni %00000010
+	;bnz _leftclicked
+	
+	; Right clicked
+	;SETISAR DINO_Y_REG
+	;lr A, S
+	;inc
+	;lr S, A
+	
+	;jmp _endloop	
+	
+_leftclicked:
 
-_small_delay.next:
+	; Left clicked
+	;SETISAR DINO_Y_REG
+	;lr A, S
+	;lr 2, A
+	;ds 2
+	;lr A, 2
+	;lr S, A
+
+	;jmp _endloop
 	
-	li 128
-_small_delay:	
-	ai	$ff
-	bnz	_small_delay
+_endloop:
+
+	jmp mainloop
+
+;---------------------------------------------------------------------------
+; Handle Dino animation
+;---------------------------------------------------------------------------
+processDinoAnimationClock:
+	SETISAR CLOCK_DINO_ANIMATION_REG
+	lr A, S
+	lr 1, A
 	
-	ds 8
-	bnz _small_delay.next
+	ds 1
+	lr A, 1
+	lr S, A
+	bnz _processDinoAnimationClockEnd
 	
+	; Reset timing
+	li TIMING_DINO_ANIMATION
+	lr S, A
+	
+	; Update when timer is 0
+	SETISAR DINO_STATE_REG
+	lr A, S
+	inc
+	lr S, A
+	
+_processDinoAnimationClockEnd:
+	pop
+	
+;---------------------------------------------------------------------------
+; Handle Dino moves
+;---------------------------------------------------------------------------
+processDinoMoveClock:
+
+	pop
+	
+;---------------------------------------------------------------------------
+; Handle Field updates
+;---------------------------------------------------------------------------
+processFieldClock:
+	SETISAR CLOCK_FIELD_REG
+	lr A, S
+	lr 1, A
+	
+	ds 1
+	lr A, 1
+	lr S, A
+	bnz _processFieldClockEnd
+	
+	; Reset timing
+	li TIMING_FIELD
+	lr S, A
+	
+	; Update when timer is 0
+	SETISAR CACTUS_X_REG
+	lr A, S
+	lr 1, A
+	ds 1
+	lr A, 1
+	lr S, A
+	
+_processFieldClockEnd:
+	pop
+	
+processBirdClock:
+	SETISAR CLOCK_BIRD_REG
+	lr A, S
+	lr 1, A
+	
+	ds 1
+	lr A, 1
+	lr S, A
+	bnz _processBirdClockEnd
+	
+	; Reset timing
+	li TIMING_BIRD
+	lr S, A
+	
+	; Update when timer is 0
+	SETISAR BIRD_X_REG
+	lr A, S
+	lr 1, A
+	ds 1
+	lr A, 1
+	lr S, A
+	
+_processBirdClockEnd:
 	pop
 
 ;---------------------------------------------------------------------------
